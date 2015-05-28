1e92628624a81fcacfe64e3a99950bac96f374de
rhaendel <rhaendel@users.noreply.github.com>
2015-05-28 23:23:34 +0200

    First end-to-end test: AuctionSniperEndToEndTest - fails, of course
    * Chapter 10
    * page 85

---
+++ src/test/java/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -0,0 +1,19 @@
+package test.endtoend.auctionsniper;
+
+import org.junit.Assert;
+
+public class ApplicationRunner {
+
+	public void startBiddingIn(FakeAuctionServer auction) {
+		// TODO Auto-generated method stub
+	}
+
+	public void showsSniperHasLostAuction() {
+		Assert.fail();
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/test/java/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -0,0 +1,28 @@
+package test.endtoend.auctionsniper;
+
+import org.junit.Assert;
+
+
+public class FakeAuctionServer {
+
+	public FakeAuctionServer(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void startSellingItem() {
+		// TODO Auto-generated method stub
+	}
+
+	public void hasReceivedJoinRequestFromSniper() {
+		Assert.fail();
+	}
+
+	public void announceClosed() {
+		// TODO Auto-generated method stub
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+	}
+
+}
