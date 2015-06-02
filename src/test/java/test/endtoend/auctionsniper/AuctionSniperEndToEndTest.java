d30760c255e417085860523bb4dde9881694c083
rhaendel <rhaendel@users.noreply.github.com>
2015-06-02 22:20:12 +0200

    The Fake Auction - A Minimal Fake Implementation
    * GOOS chapter 11
    * pages 92-93

--- src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -1,29 +1,30 @@
 package test.endtoend.auctionsniper;
 
+import org.jivesoftware.smack.XMPPException;
 import org.junit.After;
 import org.junit.Test;
 
 public class AuctionSniperEndToEndTest {
 
-	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
-	private final ApplicationRunner application = new ApplicationRunner();
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
 
-	@Test
-	public void sniperJoinsAuctionUntilAuctionCloses() {
-		auction.startSellingItem();
-		application.startBiddingIn(auction);
-		auction.hasReceivedJoinRequestFromSniper();
-		auction.announceClosed();
-		application.showsSniperHasLostAuction();
-	}
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws XMPPException {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
+    }
 
-	@After
-	public void stopAuction() {
-		auction.stop();
-	}
+    @After
+    public void stopAuction() {
+        auction.stop();
+    }
 
-	@After
-	public void stopApplication() {
-		application.stop();
-	}
+    @After
+    public void stopApplication() {
+        application.stop();
+    }
 }
