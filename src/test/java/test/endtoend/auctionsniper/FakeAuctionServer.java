1e181d3c5e7088fc539d6ca45ece10d6aad76aeb
rhaendel <rhaendel@users.noreply.github.com>
2015-06-04 15:31:39 +0200

    Changed XMPP_HOSTNAME to connect to Openfire server in docker container
    Don't expect Openfire to run on localhost. Use the docker container as
    provided by thilomack (https://github.com/thilomack/goos-infra), instead.

--- src/test/java/test/endtoend/auctionsniper/FakeAuctionServer.java
+++ src/test/java/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -14,7 +14,7 @@ public class FakeAuctionServer {
 
     public static final String ITEM_ID_AS_LOGIN = "auction-%s";
     public static final String AUCTION_RESOURCE = "Auction";
-    public static final String XMPP_HOSTNAME = "localhost";
+    public static final String XMPP_HOSTNAME = "192.168.59.103";
     private static final String AUCTION_PASSWORD = "auction";
 
     private final String itemId;
