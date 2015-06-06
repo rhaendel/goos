275ad9b2a289c809aeda773ab0f4acb4cc9897ed
rhaendel <rhaendel@users.noreply.github.com>
2015-06-06 17:40:54 +0200

    Extending the Fake Auction 3/3 - Fixing the Surprise Failure
    * GOOS chapter 12
    * page 111

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -1,5 +1,7 @@
 package auctionsniper;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.SwingUtilities;
@@ -40,6 +42,7 @@ public class Main {
     }
 
     private void joinAuction(XMPPConnection connection, String itemId) throws XMPPException {
+        disconnectWhenUICloses(connection);
         Chat chat = connection.getChatManager().createChat(auctionId(itemId, connection), new MessageListener() {
             @Override
             public void processMessage(Chat chat, Message message) {
@@ -53,6 +56,15 @@ public class Main {
         });
         this.notToBeGCd = chat;
         chat.sendMessage(JOIN_COMMAND_FORMAT);
+    }
+
+    private void disconnectWhenUICloses(final XMPPConnection connection) {
+        ui.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                connection.disconnect();
+            }
+        });
     }
 
     private static XMPPConnection connection(String hostname, String username, String password) throws XMPPException {
