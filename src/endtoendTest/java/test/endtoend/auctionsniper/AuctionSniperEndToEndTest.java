07a14dffdfa589aaa18002e16d535d9bd2917f8f
rhaendel <rhaendel@users.noreply.github.com>
2015-06-19 01:47:19 +0200

    Separated the failing end-to-end test from the successful ones
    Gradle task 'build' runs all tests except the failing one. Since the latter
    drives development and fails per definition, it makes no sense to run
    it on a CI server. We can just make sure that it compiles.

--- src/endtoendTest/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/endtoendTest/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -6,30 +6,14 @@ import org.junit.Test;
 
 public class AuctionSniperEndToEndTest {
 
-    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
-    private final ApplicationRunner application = new ApplicationRunner();
+    protected final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    protected final ApplicationRunner application = new ApplicationRunner();
 
     @Test
     public void sniperJoinsAuctionUntilAuctionCloses() throws XMPPException, InterruptedException {
         auction.startSellingItem();
         application.startBiddingIn(auction);
         auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
-        auction.announceClosed();
-        application.showsSniperHasLostAuction();
-    }
-
-    @Test
-    public void sniperMakesAHigherBidButLoses() throws Exception {
-        auction.startSellingItem();
-
-        application.startBiddingIn(auction);
-        auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
-
-        auction.reportPrice(1000, 98, "other bidder");
-        application.hasShownSniperIsBidding();
-
-        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
-
         auction.announceClosed();
         application.showsSniperHasLostAuction();
     }
