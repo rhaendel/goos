28cb439c212577a2fbaae82450cc825cfffcbe93
rhaendel <rhaendel@users.noreply.github.com>
2015-06-22 21:21:23 +0200

    Sending a Bid - The AuctionSniper Bids
    * GOOS chapter 13
    * pages 126-129

--- src/main/java/auctionsniper/SniperListener.java
+++ src/main/java/auctionsniper/SniperListener.java
@@ -6,4 +6,6 @@ public interface SniperListener extends EventListener {
 
     void sniperLost();
 
+    void sniperBidding();
+
 }
--- src/test/java/test/auctionsniper/AuctionSniperTest.java
+++ src/test/java/test/auctionsniper/AuctionSniperTest.java
@@ -6,6 +6,7 @@ import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import auctionsniper.Auction;
 import auctionsniper.AuctionSniper;
 import auctionsniper.SniperListener;
 
@@ -15,16 +16,31 @@ public class AuctionSniperTest {
 
     private final Mockery context = new Mockery();
     private final SniperListener sniperListener = context.mock(SniperListener.class);
-    private final AuctionSniper sniper = new AuctionSniper(sniperListener);
+    private final Auction auction = context.mock(Auction.class);
+    private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
 
     @Test
     public void reportsLostWhenAuctionCloses() {
         context.checking(new Expectations() {
             {
-                one(sniperListener).sniperLost();
+                atLeast(1).of(sniperListener).sniperLost();
             }
         });
 
         sniper.auctionClosed();
+    }
+
+    @Test
+    public void bidsHigherAndReportsBiddingWhenNewPriceArrives() {
+        final int price = 1001;
+        final int increment = 25;
+        context.checking(new Expectations() {
+            {
+                oneOf(auction).bid(price + increment);
+                atLeast(1).of(sniperListener).sniperBidding();
+            }
+        });
+
+        sniper.currentPrice(price, increment);
     }
 }
