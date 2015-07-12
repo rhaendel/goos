333c2d9a1b90c68bec1ae582a7a4e9c1193a20c5
rhaendel <rhaendel@users.noreply.github.com>
2015-07-12 17:24:39 +0200

    Displaying Price Details - First, a Failing Test
    * GOOS chapter 15
    * pages 152, 153

--- src/endtoendTest/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ src/endtoendTest/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,6 +1,8 @@
 package test.endtoend.auctionsniper;
 
+import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
+import static java.lang.String.valueOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import auctionsniper.ui.MainWindow;
 
@@ -17,11 +19,19 @@ public class AuctionSniperDriver extends JFrameDriver {
                 JFrameDriver.topLevelFrame( //
                         named(MainWindow.MAIN_WINDOW_NAME), //
                         showingOnScreen()), //
-                        new AWTEventQueueProber(timeoutMillis, 100));
+                new AWTEventQueueProber(timeoutMillis, 100));
     }
 
     @SuppressWarnings("unchecked")
     public void showsSniperStatus(String statusText) {
         new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void showsSniperStatus(String itemId, int lastPrice, int lastBid, String statusText) {
+        JTableDriver table = new JTableDriver(this);
+        table.hasRow(matching( //
+                withLabelText(itemId), withLabelText(valueOf(lastPrice)), //
+                withLabelText(valueOf(lastBid)), withLabelText(statusText)));
     }
 }
