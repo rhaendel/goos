d12e008541aefdd72c2394b66840d9acb4f9ae8f
rhaendel <rhaendel@users.noreply.github.com>
2015-07-26 11:12:08 +0200

    Displaying Price Details - Showing a Bidding Sniper
    * GOOS chapter 15
    * pages 156-159

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -111,8 +111,13 @@ public class Main {
     public class SniperStateDisplayer implements SniperListener {
 
         @Override
-        public void sniperBidding(SniperState sniperState) {
-            showStatus(MainWindow.STATUS_BIDDING);
+        public void sniperBidding(final SniperState state) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    ui.sniperStatusChanged(state, MainWindow.STATUS_BIDDING);
+                }
+            });
         }
 
         @Override
