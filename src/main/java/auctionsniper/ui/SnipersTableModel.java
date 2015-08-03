759037025cc0c19808c5a4ed299d0e2809003b63
rhaendel <rhaendel@users.noreply.github.com>
2015-08-03 14:13:52 +0200

    Follow Through - Object-Oriented Column
    * GOOS chapter 15
    * page 167

--- src/main/java/auctionsniper/ui/SnipersTableModel.java
+++ src/main/java/auctionsniper/ui/SnipersTableModel.java
@@ -26,18 +26,7 @@ public class SnipersTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        switch (Column.at(columnIndex)) {
-        case ITEM_IDENTIFIER:
-            return snapshot.itemId;
-        case LAST_PRICE:
-            return snapshot.lastPrice;
-        case LAST_BID:
-            return snapshot.lastBid;
-        case SNIPER_STATE:
-            return textFor(snapshot.state);
-        default:
-            throw new IllegalArgumentException("No column at " + columnIndex);
-        }
+        return Column.at(columnIndex).valueIn(snapshot);
     }
 
     public void sniperStateChanged(SniperSnapshot newSnapshot) {
@@ -50,14 +39,40 @@ public class SnipersTableModel extends AbstractTableModel {
     }
 
     public enum Column {
-        ITEM_IDENTIFIER,
-        LAST_PRICE,
-        LAST_BID,
-        SNIPER_STATE;
+
+        ITEM_IDENTIFIER {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return snapshot.itemId;
+            }
+        },
+
+        LAST_PRICE {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return snapshot.lastPrice;
+            }
+        },
+
+        LAST_BID {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return snapshot.lastBid;
+            }
+        },
+
+        SNIPER_STATE {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return SnipersTableModel.textFor(snapshot.state);
+            }
+        };
 
         public static Column at(int offset) {
             return values()[offset];
         }
+
+        abstract public Object valueIn(SniperSnapshot snapshot);
     }
 
 }
