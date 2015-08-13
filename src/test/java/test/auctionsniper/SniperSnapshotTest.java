f343b0c1f904ec53ecc0748d26f5aa27e0f2647a
rhaendel <rhaendel@users.noreply.github.com>
2015-08-13 17:06:33 +0200

    SniperStateTests and SniperSnapshotTests that are mentioned in the book
    ... but not printed explicitly.

--- src/test/java/test/auctionsniper/SniperSnapshotTest.java
+++ src/test/java/test/auctionsniper/SniperSnapshotTest.java
@@ -69,4 +69,23 @@ public class SniperSnapshotTest {
         assertFalse(snap2.equals(snap1));
     }
 
+    @Test
+    public void transitionsBetweenStates() {
+        final String itemId = "item id";
+        SniperSnapshot joining = SniperSnapshot.joining(itemId);
+        SniperSnapshot bidding = joining.bidding(123, 234);
+
+        assertEquals(new SniperSnapshot(itemId, 0, 0, SniperState.JOINING), joining);
+        assertEquals(new SniperSnapshot(itemId, 123, 234, SniperState.BIDDING), bidding);
+        assertEquals(new SniperSnapshot(itemId, 456, 234, SniperState.WINNING), bidding.winning(456));
+        assertEquals(new SniperSnapshot(itemId, 123, 234, SniperState.LOST), bidding.closed());
+        assertEquals(new SniperSnapshot(itemId, 678, 234, SniperState.WON), bidding.winning(678).closed());
+    }
+
+    @Test
+    public void comparesItemIdentities() {
+        assertTrue(SniperSnapshot.joining("item 1").isForSameItemAs(SniperSnapshot.joining("item 1")));
+        assertFalse(SniperSnapshot.joining("item 1").isForSameItemAs(SniperSnapshot.joining("item 2")));
+    }
+
 }
