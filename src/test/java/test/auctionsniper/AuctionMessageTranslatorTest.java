215c19c25a41c7192e5e27ff97d3919b8f5622fc
rhaendel <rhaendel@users.noreply.github.com>
2019-01-08 22:21:14 +0100

    Remove a deprecation warning in the unit test class: Rule instead of RunWith

--- src/test/java/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ src/test/java/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -1,32 +1,28 @@
 package test.auctionsniper;
 
+import auctionsniper.AuctionEventListener;
+import auctionsniper.AuctionMessageTranslator;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
 import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import auctionsniper.AuctionEventListener;
-import auctionsniper.AuctionMessageTranslator;
-
-@SuppressWarnings("deprecation")
-@RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
 
     public static final Chat UNUSED_CHAT = null;
-    private final Mockery context = new Mockery();
+
+    @Rule
+    public final JUnitRuleMockery context = new JUnitRuleMockery();
     private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
     private final AuctionMessageTranslator translator = new AuctionMessageTranslator(listener);
 
     @Test
     public void notifiesAuctionClosedWhenMessageReceived() {
-        context.checking(new Expectations() {
-            {
-                oneOf(listener).auctionClosed();
-            }
-        });
+        context.checking(new Expectations() {{
+            oneOf(listener).auctionClosed();
+        }});
 
         Message message = new Message();
         message.setBody("SOLVersion: 1.1; Event: CLOSE;");
@@ -36,11 +32,9 @@ public class AuctionMessageTranslatorTest {
 
     @Test
     public void notifiesBidDetailsWhenCurrentPriceMessageReceived() {
-        context.checking(new Expectations() {
-            {
-                exactly(1).of(listener).currentPrice(192, 7);
-            }
-        });
+        context.checking(new Expectations() {{
+            exactly(1).of(listener).currentPrice(192, 7);
+        }});
 
         Message message = new Message();
         message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;");
